bebd80df01db2a6ebd6a6cef34b53dec81fe8b54
ptyo.wizard <ptyo.wizard@gmail.com>
2021-08-06 16:55:32 -0500

    add estructura base de layer__greeting

---
+++ js/home_anime.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const ball = document.querySelector('.layer__point');
+    const greeting = document.querySelector('.layer__title');
+    const layer = document.querySelector('.layer');
+    const base_layer = document.querySelector('.home-sect');
+    const title = document.querySelector('.home-sect__text');
+    const projects = document.querySelectorAll('div.projects-frame');
+    const inputMenu = document.querySelector('#menu');
+    const legendProject = document.querySelector('.projects__legend');
+    const socialIcons = document.querySelector('.home-sect__social');
+    const skillsIcons = document.querySelector('.home-sect__skills');
+
+
+});
