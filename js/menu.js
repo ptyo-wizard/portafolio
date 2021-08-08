98291f7f51e157b18f9884bb90769861bd3c1d28
ptyo.wizard <ptyo.wizard@gmail.com>
2021-08-08 10:52:49 -0500

    add layer__nav,  acompaña a menu

--- js/menu.js
+++ js/menu.js
@@ -9,29 +9,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const projects = document.querySelector('#_projects');
     const contact = document.querySelector('#_contact');
 
+    //
+
     iconMenu.addEventListener('click',()=>{
         heroNav.classList.toggle('show-menu');   
+        //layerNav.classList.toggle('show-menu');
     });
     
     menuClosed.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        
     });
 
     home.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        
     });
     about.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        
     });
     skills.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        layerNav.classList.toggle('show-menu');  
     });
     projects.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        
     });
 
     contact.addEventListener('click',()=>{
-        heroNav.classList.toggle('show-menu');   
+        heroNav.classList.toggle('show-menu'); 
+        
     });
 
 });
